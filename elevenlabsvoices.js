48334fddef571cf09d138001dc78aad1b5777cd7
Eric Kern <ericrkern@yahoo.com>
2024-12-08 12:29:04 -0500

    update elevenlabsvoices.js
    Added comments and cleaned up the code.

--- elevenlabsvoices.js
+++ elevenlabsvoices.js
@@ -1,15 +1,25 @@
 import { ElevenLabsClient, play } from "elevenlabs";
 
+//Update the apiKey and Voice ID with your own here:
+const apiKey = "sk_9caa3d09d80b88b50001a7da829d98b3774a9c418b46886c";
+const voice = "xC4J856jWFu58QWmjnxD";
+
+// Create a new ElevenLabsClient using the apiKey from above
 const elevenlabs = new ElevenLabsClient({
-    apiKey: "sk_9caa3d09d80b88b50001a7da829d98b3774a9c418b46886c", 
+    apiKey, 
 });
 
+// Set the text to convert to voice using the passed argument
 const text = process.argv[2];
 
+// generate the voice from the text using the desired paramenters
+// additional parameters can be added below and are defined here:
+// https://elevenlabs.io/docs/api-reference/text-to-speech
 const audio = await elevenlabs.generate({
-    voice: "xC4J856jWFu58QWmjnxD",
+    voice,
     text,
     model_id: "eleven_multilingual_v2",
 });
 
+// Pause while the audio plays through ffplay.exe
 await play(audio);
